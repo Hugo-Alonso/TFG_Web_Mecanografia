506ccfc627ab79b6c221c7ab4385e61728a4bf60
Hugo-Alonso <hugoalonsotoledo12@gmail.com>
2025-03-19 19:30:29 +0100

    Endpoint registro (signup) creado y probado. #9

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -3,7 +3,50 @@ import User from "../model/user.model.js";
 import bcrypt from "bcryptjs";
 
 export const signup = async (req, res) => {
-    res.send("signup route");
+    // Collect data from the request 
+    const { username, email, password } = req.body;
+
+    try {
+        if (!username || !email || !password) {
+            return res.status(400).json({ message: "All fields are required"});
+        }
+
+        const emailExists = await User.findOne({email});
+
+        if (emailExists) return res.status(400).json({ message: "Email already exists"});
+
+        if (password.length < 6) {
+            return res.status(400).json({ message: "Password must be at least 6 characters"});
+        }
+
+        // Hash Password before store it at the DB
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        // Create new User
+        const newUser = new User({
+            email: email,
+            username: username,
+            password: hashedPassword
+        })
+
+        if (newUser) {
+            generateToken(newUser._id, res);
+            await newUser.save();
+
+            res.status(201).json({
+                _id: newUser._id,
+                email: newUser.email,
+                username: newUser.username,
+            });
+        } else {
+            res.status(400).json({ message: "Invalid user data" });
+        }
+
+    } catch (error) {
+        console.log("Error in signup controller", error.message);
+        res.status(500).json({ message: "Internal Server Error"});
+    }
 };
 
 export const login = (req, res) => {
