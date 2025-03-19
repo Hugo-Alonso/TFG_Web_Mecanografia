4b22886c4d0daba748529768396d5db1b7485ab0
Hugo-Alonso <hugoalonsotoledo12@gmail.com>
2025-03-19 18:40:02 +0100

    Rutas aplicación configuradas V1. #5

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { HomePage } from './pages/HomePage';
 import { SignupPage } from './pages/SignupPage';
@@ -14,7 +13,26 @@ function App() {
   return (
     <>
       <NavBar />
-    
+      
+      <br />
+
+      <div>
+        <Routes >
+          {/* Auth Routes */}
+          <Route path="/" element={ <HomePage /> } />
+          <Route path="/signup" element={ <SignupPage /> } />
+          <Route path="/login" element={ <LoginPage /> } />
+
+          {/* User Routes */}
+          <Route path="/profile" element={ <ProfilePage /> } />
+          <Route path="/settings" element={ <SettingsPage /> } />
+        
+          {/* Tests Routes */}
+          <Route path="/timetest" element={ <TimetestPage /> } />
+          <Route path="/wordtest" element={ <WordtestPage /> } />
+          <Route path="/customtest" element={ <CustomtestPage /> } />
+        </Routes>
+      </div>
     </>
   )
 }
